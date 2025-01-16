c481dc19b2c508aef40a70439019cd77d691e0d1
NDK <80230626+mark3473@users.noreply.github.com>
2025-01-16 18:11:54 +0900

    [Gold III] Title: 움직이는 미로 탈출, Time: 68 ms, Memory: 11592 KB -BaekjoonHub

--- 백준/Gold/16954. 움직이는 미로 탈출/움직이는 미로 탈출.java
+++ 백준/Gold/16954. 움직이는 미로 탈출/움직이는 미로 탈출.java
@@ -7,6 +7,7 @@ public class Main {
 
     static String[] map = new String[8];
     static String cleanLine = "........";
+    static int[][] visited = new int[8][8];
 
     static class Point{
         int i;
@@ -29,23 +30,28 @@ public class Main {
 
         Queue<Point> q = new ArrayDeque<>();
         q.add(new Point(7,0, 0));
+        visited[7][0] = 0;
 
         while(!q.isEmpty()){
             Point cur = q.poll();
-
-            if(cur.i == 0) {
-                System.out.println(1);
-                return;
-            }
 
             for(int d=0; d<8; d++){
                 int ni = cur.i + di[d];
                 int nj = cur.j + dj[d];
 
-                if(0<=ni&&ni<8 && 0<=nj&&nj<8){
+                if(ni==0 && 0<=nj&&nj<8){
+                    String line = ni-cur.step<0? cleanLine : map[ni-cur.step];
+                    if(line.charAt(nj)=='.'){
+                        System.out.println(1);
+                        return;
+                    }
+                }
+
+                if(0<=ni&&ni<8 && 0<=nj&&nj<8 && visited[ni][nj]!=cur.step+1){
                     String line = ni-cur.step<0? cleanLine : map[ni-cur.step];
                     String above = ni-cur.step-1<0? cleanLine : map[ni-cur.step-1];
                     if(line.charAt(nj)=='.' && above.charAt(nj)=='.'){
+                        visited[ni][nj] = cur.step+1;
                         q.add(new Point(ni, nj, cur.step+1));
                     }
                 }
