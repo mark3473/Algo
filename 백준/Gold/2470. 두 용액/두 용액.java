2bf42323504c0e0c62feec707d7d292130fa65d9
NDK <80230626+mark3473@users.noreply.github.com>
2025-02-17 21:46:58 +0900

    [Gold V] Title: 두 용액, Time: 256 ms, Memory: 31612 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        long[] liq = new long[N];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            liq[i] = Long.parseLong(st.nextToken());
+        }
+        Arrays.sort(liq);
+
+        int left = 0;
+        int right = N-1;
+        long tmp = 2000000000;
+        long ans1=0;
+        long ans2=0;
+        while(left < right){
+            long sum = liq[left] + liq[right];
+            if(Math.abs(tmp) > Math.abs(sum)){
+                ans1 = liq[left];
+                ans2 = liq[right];
+                tmp = sum;
+            }
+
+            if(sum>0){ // 합이 너무 크면
+                right--; // 덜 큰 것을 더해야함
+            }
+            else if(sum<0){ // 합이 너무 작으면
+                left++; // 더 큰 것을 더해야함
+            }
+            else break;
+        }
+
+        System.out.println(ans1 + " " + ans2);
+    }
+}
