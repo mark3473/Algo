01d68be039d89822933e850bd97818dfa8bfce16
NDK <80230626+mark3473@users.noreply.github.com>
2025-05-28 19:09:12 +0900

    [Gold II] Title: 오름세, Time: 204 ms, Memory: 27896 KB -BaekjoonHub

--- 백준/Gold/3745. 오름세/오름세.java
+++ 백준/Gold/3745. 오름세/오름세.java
@@ -33,8 +33,9 @@ public class Main {
         while(left<right){
             int mid = (left + right)/2;
 
-            if(lis[mid]>=val) {
-                right = mid;
+            // val보다 작은 수 중 가장 큰 수의 오른쪽에 위치 시켜야함
+            if(lis[mid]>=val) { // 따라서 lis[mid]값이 val보다 크거나 같으면 일단 그 위치를 용의선상에 놓음
+                right = mid; // 그래서 right을 mid값으로 하고 나중에 return right을 하는거임
             }
             else {
                 left = mid+1;
