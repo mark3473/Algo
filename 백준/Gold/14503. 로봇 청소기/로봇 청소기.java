9a3ef1f66c82c7f9e5aac6f039f9fe3731b30f4f
NDK <80230626+mark3473@users.noreply.github.com>
2025-09-21 15:57:10 +0900

    [Gold V] Title: 로봇 청소기, Time: 68 ms, Memory: 11852 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -2,27 +2,31 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    // 북동남서
     static int[] di = {-1, 0, 1, 0};
     static int[] dj = {0, 1, 0, -1};
 
-    static int Ri, Rj, Rd;
+    static class Point{
+        int i;
+        int j;
 
-    public static void main(String[] args) throws Exception {
+        Point(int i, int j){
+            this.i = i;
+            this.j = j;
+        }
+    }
+
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        st = new StringTokenizer(br.readLine());
-        // 방의 크기
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-
         int[][] map = new int[N][M];
+        boolean[][] visited = new boolean[N][M];
 
         st = new StringTokenizer(br.readLine());
-        Ri = Integer.parseInt(st.nextToken());
-        Rj = Integer.parseInt(st.nextToken());
-        Rd = Integer.parseInt(st.nextToken());
+        Point robot = new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+        int dir = Integer.parseInt(st.nextToken()); // 바라보는 방향
 
         for(int i=0; i<N; i++){
             st = new StringTokenizer(br.readLine());
@@ -31,40 +35,36 @@ public class Main {
             }
         }
 
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[] {Ri, Rj, Rd});
         int ans = 0;
-        while(!q.isEmpty()){
-            int[] cur = q.poll();
-            if(map[cur[0]][cur[1]]!=2){
-                map[cur[0]][cur[1]]=2;
+        while(true){
+            boolean flag = false;
+            // 1. 현재칸 청소
+            if(!visited[robot.i][robot.j]){
                 ans++;
+                visited[robot.i][robot.j] = true;
             }
-
-            // 주변 칸의 청소 상태 확인
+            // 2. 주위 칸에 청소안한 칸 확인
+            // 있으면 반시계 회전 후 앞칸 청소 안됐으면 이동
             for(int d=0; d<4; d++){
-                int nd = (cur[2]-d+3)%4;
-                int ni = cur[0]+di[nd];
-                int nj = cur[1]+dj[nd];
-                if(0<=ni&&ni<N && 0<=nj&&nj<M){
-                    // 청소되어있지 않는 칸이면
-                    if(map[ni][nj]==0){
-                        // 반시계 돌고
-                        // 그 방향이 청소되지 않은 칸이면 전진
-                        q.add(new int[] {ni, nj, nd});
-                        break;
-                    }
+                int ni = robot.i + di[(3+dir-d)%4];
+                int nj = robot.j + dj[(3+dir-d)%4];
+                if(0<=ni&&ni<N && 0<=nj&&nj<M && map[ni][nj]==0 && !visited[ni][nj]) {
+                    flag = true;
+                    robot.i = ni;
+                    robot.j = nj;
+                    dir = (3+dir-d)%4;
+                    break;
                 }
             }
-            
-            // 주변이 다 청소한 상태이면
-            if(q.isEmpty()){
-                int ni = cur[0] - di[cur[2]];
-                int nj = cur[1] - dj[cur[2]];
-                if(0<=ni&&ni<N && 0<=nj&&nj<M && map[ni][nj]!=1){
-                    q.add(new int[] {ni, nj, cur[2]});
-                }
-                else break;
+            // 3. 주위 칸에 청소안한 칸 없으면 후진
+            // 4. 후진 못하면 끝
+            if(!flag){
+                int ni = robot.i - di[dir];
+                int nj = robot.j - dj[dir];
+
+                if(ni<0||ni>=N || nj<0||nj>=M || map[ni][nj]==1) break;
+                robot.i = ni;
+                robot.j = nj;
             }
         }
 
