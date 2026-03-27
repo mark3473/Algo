85dad90b03694cad218016fb67eeacab9503e573
NDK <80230626+mark3473@users.noreply.github.com>
2026-03-27 17:33:49 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 308 ms, Memory: 29952 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -0,0 +1,97 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static class History{
+        int time;
+        int idx;
+
+        History(int t, int c) {
+            this.time = t;
+            this.idx = c;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        if(N==K) {
+            System.out.println(0);
+            return;
+        }
+        
+        if(K==0) {
+            System.out.println(N);
+            return;
+        }
+
+        Set<Integer> sub = new HashSet<>();
+
+        int tmp = 0;
+
+        if(K%2==0){
+            for(int k=K; k%2==0; k/=2){
+                if(k==N) {
+                    System.out.println(0);
+                    return;
+                }
+                sub.add(k);
+            }
+        }
+
+        else{
+            tmp=1;
+            for(int k=K+1; k%2==0; k/=2){
+                if(k==N) {
+                    System.out.println(1);
+                    return;
+                }
+                sub.add(k);
+            }
+
+            for(int k=K-1; k%2==0; k/=2){
+                if(k==N) {
+                    System.out.println(1);
+                    return;
+                }
+                if(sub.contains(k)) break;
+                sub.add(k);
+            }
+        }
+
+        PriorityQueue<History> pq = new PriorityQueue<>((o1, o2)-> o1.time-o2.time);
+        pq.add(new History(0, N));
+
+        boolean[] visited = new boolean[200001];
+
+        while(!pq.isEmpty()){
+            History cur = pq.poll();
+            int time = cur.time;
+            int idx = cur.idx;
+
+            if(sub.contains(idx)) {
+                System.out.println(time+tmp);
+                return;
+            }
+
+            if(visited[idx]) continue;
+            visited[idx] = true;
+
+            if(idx*2<=200000){
+                pq.add(new History(time, idx*2));
+            }
+
+            if(idx>0) {
+                pq.add(new History(time+1, idx-1));
+            }
+
+            if(idx+1<=K){
+                pq.add(new History(time+1, idx+1));
+            }
+        }
+    }
+}
