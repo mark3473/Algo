ab3964cc8cf4567b5406f41375204b06d42229ab
NDK <80230626+mark3473@users.noreply.github.com>
2026-03-28 17:41:44 +0900

    [Gold V] Title: 택배 배송, Time: 636 ms, Memory: 278384 KB -BaekjoonHub

---
+++ 백준/Gold/5972. 택배 배송/택배 배송.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static class Node{
+        int idx;
+        int cow;
+
+        Node(int i, int c){
+            this.idx = i;
+            this.cow = c;
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken()); // 헛간
+        int M = Integer.parseInt(st.nextToken()); // 길
+
+        ArrayList<Node>[] graph = new ArrayList[N];
+        for(int i=0; i<N; i++) graph[i] = new ArrayList<>();
+
+        for(int i=0; i<M; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken())-1;
+            int b = Integer.parseInt(st.nextToken())-1;
+            int c = Integer.parseInt(st.nextToken());
+
+            graph[a].add(new Node(b, c));
+            graph[b].add(new Node(a, c));
+        }
+
+        int[] minD = new int[N];
+        for(int i=1; i<N; i++) minD[i] = Integer.MAX_VALUE;
+        Queue<Integer> q = new LinkedList<>();
+        q.add(0);
+        while(!q.isEmpty()){
+            int cur = q.poll();
+
+            for(Node next : graph[cur]){
+                if(minD[cur]+next.cow < minD[next.idx]){
+                    minD[next.idx] = minD[cur]+next.cow;
+                    q.add(next.idx);
+                }
+            }
+        }
+
+        System.out.println(minD[N-1]);
+    }
+}
